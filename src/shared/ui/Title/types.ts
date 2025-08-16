989c35c2f29e87d66da5fc1eee60f88d34c3351c
Aleksandra Provorova <sashaprovora@gmail.com>
2025-08-16 20:19:24 +0300

    fix: добавила h4

--- src/shared/ui/Title/types.ts
+++ src/shared/ui/Title/types.ts
@@ -1,8 +1,8 @@
 import { HTMLAttributes } from 'react';
 
-export type TitleTag = 'h1' | 'h2' | 'h3';
+export type TitleTag = 'h1' | 'h2' | 'h3' | 'h4';
 
-export type TitleSize = 'lg' | 'md' | 'sm';
+export type TitleSize = 'xl' | 'lg' | 'md' | 'sm';
 
 export interface ITitleProps extends HTMLAttributes<HTMLHeadingElement> {
   as: TitleTag;
