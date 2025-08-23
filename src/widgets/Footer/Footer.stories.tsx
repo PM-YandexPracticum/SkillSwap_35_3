285a26cd1ec288866a1c6c56ab6b67f532e2f320
NataliaMuratova1979 <mno@mail.ru>
2025-08-23 19:53:25 +0300

    прописаны стили по макету

--- src/widgets/Footer/Footer.stories.tsx
+++ src/widgets/Footer/Footer.stories.tsx
@@ -1,12 +1,11 @@
 import React from 'react';
 import { Meta, Story } from '@storybook/react';
-import { MemoryRouter } from 'react-router-dom';
-
 import { Footer } from './Footer';
 import { FooterProps } from './types';
+import { MemoryRouter } from 'react-router-dom';
 
 export default {
-  title: 'Shared/Footer',
+  title: 'shared/Footer',
   component: Footer,
   argTypes: {
     show: { control: 'boolean' },
@@ -15,16 +14,16 @@ export default {
     height: { control: 'text' },
     padding: { control: 'text' },
     onClick: { action: 'clicked' },
-    style: { control: 'object' }
+    style: { control: 'object' },
+    children: { control: 'text' }
   },
-  // Добавляем декоратор, который оборачивает все истории в MemoryRouter
   decorators: [
     (Story) => (
       <MemoryRouter>
         <Story />
       </MemoryRouter>
-    ),
-  ],
+    )
+  ]
 } as Meta;
 
 // Базовая история
@@ -33,41 +32,11 @@ const Template: Story<FooterProps> = (args) => <Footer {...args} />;
 export const Default = Template.bind({});
 Default.args = {
   show: true,
-  className: '',
-  style: {},
-  children: null,
   isSticky: false,
-  backgroundColor: '#f0f0f0',
-  height: '100px',
-  padding: '10px',
-  onClick: () => alert('Footer clicked!'),
-  id: 'footer-id',
-  ariaLabel: 'Футер сайта',
-  'data-cy': 'footer'
-};
-
-// История без отображения (скрыт)
-export const Hidden = Template.bind({});
-Hidden.args = {
-  show: false
-};
-
-// История с закрепленным футером
-export const Sticky = Template.bind({});
-Sticky.args = {
-  show: true,
-  isSticky: true,
-  backgroundColor: '#333',
-  height: '80px'
-};
-
-// История с кастомными стилями
-export const CustomStyles = Template.bind({});
-CustomStyles.args = {
-  show: true,
-  style: {
-    backgroundColor: '#ffcc00',
-    borderTopLeftRadius: '10px',
-    borderTopRightRadius: '10px'
-  }
+  backgroundColor: undefined,
+  height: undefined,
+  padding: undefined,
+  className: '',
+  style: {}, // Можно оставить пустым или добавить стили по необходимости
+  children: null
 };
