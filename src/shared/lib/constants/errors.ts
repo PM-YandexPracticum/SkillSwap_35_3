f527dcc3143f49428392334a386c13c518a83fff
Slava-ZV <nadezhdadaniarova@yandex.ru>
2025-08-24 22:13:14 +0300

    feat(errors): добавлены ресурсы для отображения ошибок 404 и 500
    - картинки в формате svg
    - названия и описания ошибок для вывода пользователю

---
+++ src/shared/lib/constants/errors.ts
@@ -0,0 +1,23 @@
+import { Error404Image, Error500Image } from '@/shared/assets/images/errors';
+
+type ErrorImage = React.FC<React.SVGProps<SVGSVGElement>>;
+
+type ErrorData = {
+  title: string;
+  description: string;
+  image: ErrorImage;
+};
+
+export const errors: Record<number, ErrorData> = {
+  404: {
+    title: 'Страница не найдена',
+    description:
+      'К сожалению, эта страница недоступна. Вернитесь на главную страницу или попробуйте позже',
+    image: Error404Image
+  },
+  500: {
+    title: 'На сервере произошла ошибка',
+    description: 'Попробуйте позже или вернитесь на главную страницу',
+    image: Error500Image
+  }
+};
