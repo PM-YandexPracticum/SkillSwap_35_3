a038e839d65506e3bd62e7ff34f14225dd7450f9
Maksim <kotyukov.maks@mail.ru>
2025-08-31 02:55:17 +0300

    feat(ui/input): доработан компонент input, дописаны стили, типы и storybook

--- src/shared/ui/Input/input.tsx
+++ src/shared/ui/Input/input.tsx
@@ -1,7 +1,8 @@
 import { InputProps } from './type';
 import clsx from 'clsx';
-import { forwardRef } from 'react';
+import { forwardRef, useState } from 'react';
 import styles from './Input.module.css';
+import { Icon } from '../Icon';
 
 export const Input = forwardRef<HTMLInputElement, InputProps>(
   (props: InputProps, ref) => {
@@ -11,8 +12,16 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
       icon,
       search,
       iconPosition,
+      message,
+      error,
       ...rest
     } = props;
+
+    const [passwordVisible, setPasswordVisible] = useState(false);
+
+    const inputType = type === 'password' && passwordVisible ? 'text' : type;
+
+    const showToggle = type === 'password';
 
     const iconContainerClasses = clsx(styles.input__icon_container, {
       [styles.input__icon_left]: iconPosition === 'left',
@@ -20,14 +29,44 @@ export const Input = forwardRef<HTMLInputElement, InputProps>(
     });
 
     return (
-      <div className={clsx(styles.input__wrapper, className)}>
-        <input
-          type={type}
-          ref={ref}
-          className={clsx(styles.input, { [styles.input_search]: search })}
-          {...rest}
-        />
-        {icon && <div className={iconContainerClasses}>{icon}</div>}
+      <div className={styles['input__container']}>
+        <div className={clsx(styles.input__wrapper, className)}>
+          <input
+            type={inputType}
+            ref={ref}
+            className={clsx(
+              styles.input,
+              { [styles.input_search]: search },
+              { [styles.input__error]: error }
+            )}
+            {...rest}
+          />
+          {icon && !showToggle && (
+            <div className={iconContainerClasses}>{icon}</div>
+          )}
+          {showToggle && (
+            <button
+              type='button'
+              className={clsx(
+                iconContainerClasses,
+                styles['input__toggle-button']
+              )}
+              onClick={() => setPasswordVisible((prev) => !prev)}
+              aria-label={passwordVisible ? 'Скрыть пароль' : 'Показать пароль'}
+            >
+              <Icon name='eye-icon' />
+            </button>
+          )}
+        </div>
+        {message ? (
+          <span
+            className={clsx(styles['input__message'], {
+              [styles['input__error-message']]: error
+            })}
+          >
+            {message}
+          </span>
+        ) : null}
       </div>
     );
   }
